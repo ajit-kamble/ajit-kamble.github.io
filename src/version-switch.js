99b244c99f68f080ce352fed168226c41c3d0b8f
Ajit Kamble <ajit1721@gmail.com>
2024-11-24 15:37:01 -0600

    changes to add version switch component

--- src/version-switch.js
+++ src/version-switch.js
@@ -1,8 +1,23 @@
-
+import { Box, Typography } from "@mui/material";
 import { AppV1 } from "./v1";
 
 function VersionSwitch() {
-  return <AppV1 />;
+  const template = process.env.REACT_APP_TEMPLATE;
+  let App = null;
+  switch (template) {
+    case "1":
+      App = <AppV1 />;
+      break;
+    default:
+      App = (
+        <Box m={8} width="100%" textAlign="center">
+          <Typography fontWeight="bold" fontSize="25px" variant="overline">
+          Template Not Configured
+          </Typography>
+        </Box>
+      );
+  }
+  return App;
 }
 
 export default VersionSwitch;
